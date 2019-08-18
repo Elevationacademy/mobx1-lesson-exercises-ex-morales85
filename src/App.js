7fe1b6b5410e131239706c0b8e97df7fe89fc66a
morales85 <mor4les85@gmail.com>
2019-08-18 19:45:22 +0300

    excercises done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 import './App.css';
 import Item from './components/Item';
-
+import DevTools from 'mobx-react-devtools';
 
 @observer
 class App extends Component {
@@ -17,11 +17,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        < DevTools />
         <input onChange = {this.handleChange}/>
-        <button onClick = {this.addItem}>Add</button>
-      {/* your code here
-          You should map each grocery item into an Item component  
-      */}  
+        <button onClick = {this.addItem}>Add</button>       
+      {this.props.store.list.map((i, ind) => <Item item = {i} key = {ind} store={this.props.store}/>)}  
       </div>
     );
   }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react'
+import '../App.css';
+import { timingSafeEqual } from 'crypto';
+
 
 @observer
 class Item extends Component {
-    checkItem = () => {
-        //your code here
-      }
+    checkItem = (e) => {
+      this.props.store.checkItem(e.target.value)
+    }
       editItem = () => {
-        //your code here
+        let newLocation = prompt("Change location", "Super Sell");
+        this.props.store.editItem(this.props.item.name, newLocation)
       }
       deleteItem = () => {
-        //your code here
+        console.log(this.props.item)
+        this.props.store.deleteItem(this.props.item.name)
       }
     render() {
+      let item = this.props.item
         return (
-            <div className = "">
-      {/*   your code here
-            each item should be in an input checkbox
-            it should display the item name and location
-            it should have both an edit button and a delete button
-      */} 
-            </div>)
+              <div className={item.completed ? 'crossed' : null}>
+                <input type="checkbox" onClick = {this.checkItem} value={item.name}/>{item.name}  {item.location}
+                <button className='editButton' onClick={this.editItem}>Edit</button>
+                <button className='deleteButton' onClick={this.deleteItem}>Delete</button>
+              </div>
+
+            )
     }
 }
 
